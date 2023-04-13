c4ce3ab9371beac358bc86c23647e770cb543611
zypherscript <kitti.kthp@gmail.com>
2023-04-13 17:53:31 +0700

    aop swag

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,8 +4,13 @@ import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 import java.util.Collection;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.StreamSupport;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +19,10 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
@@ -66,6 +73,16 @@ class CustomerController {
   Collection<Customer> customersByName(@Argument String name) {
     return customerRepository.findByName(name);
   }
+
+  @GetMapping("/testSome/{str}")
+  void callTestSome(@PathVariable String str) {
+    testSome(str);
+  }
+
+  @SneakyThrows
+  void testSome(String str) {
+    TimeUnit.SECONDS.sleep(2);
+  }
 }
 
 //https://www.boredapi.com/api/
@@ -114,6 +131,20 @@ class WebsiteView extends VerticalLayout {
     var customers = StreamSupport.stream(customerRepository.findAll()
         .spliterator(), false).toList();
     grid.setItems(customers);
+  }
+}
+
+@Aspect
+@Component
+class MyAspect {
+
+  @Around("execution(* com.example.demo.*.test*(String))")
+  public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
+    long start = System.currentTimeMillis();
+    Object result = joinPoint.proceed();
+    long elapsedTime = System.currentTimeMillis() - start;
+    System.out.println(joinPoint.getSignature().getName() + " executed in " + elapsedTime + "ms");
+    return result;
   }
 }
 
