71b6cfa7d46bccab8f017d51b03e7d2ad8956bc6
zypherscript <kitti.kthp@gmail.com>
2023-05-21 04:05:52 +0700

    vthread preview

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.http.HttpStatus;
@@ -17,6 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 class CustomerController {
 
   private final CustomerRepository customerRepository;
@@ -26,6 +28,30 @@ class CustomerController {
   Iterable<Customer> customers() {
     return customerRepository.findAll();
   }
+
+//  @GetMapping("/test/vthread")
+//  void testVthread() {
+//    var start = Instant.now();
+//
+//    final AtomicInteger atomicInteger = new AtomicInteger();
+//    Runnable runnable = () -> {
+//      try {
+//        Thread.sleep(Duration.ofSeconds(1));
+//      } catch (Exception ignored) {
+//      }
+//      log.info("Work Done - " + atomicInteger.incrementAndGet());
+//    };
+//
+//    try (var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+//      for (int i = 0; i < 10_000; i++) {
+//        executor.submit(runnable);
+//      }
+//    }
+//
+//    var finish = Instant.now();
+//    long timeElapsed = Duration.between(start, finish).toMillis();
+//    System.out.println("Total elapsed time : " + timeElapsed);
+//  }
 
   @QueryMapping
     //graphql
