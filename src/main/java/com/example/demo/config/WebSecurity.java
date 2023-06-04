6678b547af4a62d9a920b858a62216a184ed2cee
zypherscript <kitti.kthp@gmail.com>
2023-06-05 03:51:14 +0700

    up

--- src/main/java/com/example/demo/config/WebSecurity.java
+++ src/main/java/com/example/demo/config/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.example.demo.config;
 
+import java.util.Map;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -7,6 +8,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,12 +25,29 @@ public class WebSecurity {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
         .csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(authorize ->
-            authorize.requestMatchers("/actuator/**").permitAll())
-        .authorizeHttpRequests(authorize ->
-            authorize.anyRequest().authenticated())
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers("/actuator/**").permitAll()
+            .anyRequest().authenticated()
+        )
         .httpBasic(Customizer.withDefaults());
     return http.build();
+  }
+
+  @Bean
+  DelegatingPasswordEncoder passwordEncoder() {
+    var bcrypt = new BCryptPasswordEncoder(10); //check this later bruh
+    Map<String, PasswordEncoder> passwordEncoders = Map.of("bcrypt", bcrypt);
+    return new DelegatingPasswordEncoder("bcrypt", passwordEncoders);
+  }
+
+  @Bean
+  UserDetailsService users() {
+    return new InMemoryUserDetailsManager(
+        User.withUsername("user")
+            .password("{bcrypt}$2a$10$73yinqB.52oyN682b.Aew.39orzbjhBNZfxXIoW0q4N3bYXXQDqJS")
+            .roles("USER")
+            .build()
+    );
   }
 
 }
