defa2ca945722fe88da0634964c765654fb2a4ea
zypherscript <kitti.kthp@gmail.com>
2023-06-05 18:00:36 +0700

    session with redis

--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,7 +1,9 @@
 package com.example.demo.entity;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+@Table
 public record Customer(@Id Integer id, String name) {
 
 }
